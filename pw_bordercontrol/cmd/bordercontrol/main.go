893d4e8159652307fd6ce0b5378ec8c5ea33da14
Mike Nye <mike.nye@gmail.com>
2023-05-16 18:44:04 +0800

    .

--- pw_bordercontrol/cmd/bordercontrol/main.go
+++ pw_bordercontrol/cmd/bordercontrol/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"crypto/tls"
+	"net"
 	"net/url"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -81,120 +83,120 @@ func updateFeederDB(ctx *cli.Context, updateFreq time.Duration) {
 	}
 }
 
-// func clientConnection(ctx *cli.Context, conn net.Conn, tlsConfig *tls.Config) {
-// 	// handles incoming connections
+func clientConnection(ctx *cli.Context, conn net.Conn, tlsConfig *tls.Config) {
+	// handles incoming connections
 
-// 	// TODO: need a way to kill a client connection if the UUID is no longer valid (ie: feeder banned)
+	// TODO: need a way to kill a client connection if the UUID is no longer valid (ie: feeder banned)
 
-// 	cLog := log.With().Logger()
+	cLog := log.With().Logger()
 
-// 	var (
-// 		sendRecvBufferSize  = 1024
-// 		clientAuthenticated = false
-// 		clientApiKey        uuid.UUID
-// 		err                 error
-// 	)
+	var (
+		sendRecvBufferSize  = 1024
+		clientAuthenticated = false
+		clientApiKey        uuid.UUID
+		err                 error
+	)
 
-// 	defer conn.Close()
+	defer conn.Close()
 
-// 	// update log context with client IP
-// 	remoteIP := net.ParseIP(strings.Split(conn.RemoteAddr().String(), ":")[0])
-// 	cLog = cLog.With().IPAddr("client", remoteIP).Logger()
+	// update log context with client IP
+	remoteIP := net.ParseIP(strings.Split(conn.RemoteAddr().String(), ":")[0])
+	cLog = cLog.With().IPAddr("client", remoteIP).Logger()
 
-// 	cLog.Debug().Msgf("connection established")
-// 	defer cLog.Debug().Msgf("connection closed")
+	cLog.Debug().Msgf("connection established")
+	defer cLog.Debug().Msgf("connection closed")
 
-// 	buf := make([]byte, sendRecvBufferSize)
-// 	for {
+	buf := make([]byte, sendRecvBufferSize)
+	for {
 
-// 		// read data
-// 		_, err = conn.Read(buf)
-// 		if err != nil {
-// 			if err.Error() == "tls: first record does not look like a TLS handshake" {
-// 				cLog.Warn().Msg(err.Error())
-// 			} else if err.Error() == "EOF" {
-// 				if clientAuthenticated {
-// 					cLog.Info().Msg("client disconnected")
-// 				}
-// 			} else {
-// 				cLog.Err(err).Msg("conn.Read")
-// 			}
-// 			break
-// 		}
+		// read data
+		_, err = conn.Read(buf)
+		if err != nil {
+			if err.Error() == "tls: first record does not look like a TLS handshake" {
+				cLog.Warn().Msg(err.Error())
+			} else if err.Error() == "EOF" {
+				if clientAuthenticated {
+					cLog.Info().Msg("client disconnected")
+				}
+			} else {
+				cLog.Err(err).Msg("conn.Read")
+			}
+			break
+		}
 
-// 		// When the first data is sent, the TLS handshake should take place.
-// 		// Accordingly, we need to track the state...
-// 		if !clientAuthenticated {
+		// When the first data is sent, the TLS handshake should take place.
+		// Accordingly, we need to track the state...
+		if !clientAuthenticated {
 
-// 			// check TLS handshake
-// 			tlscon := conn.(*tls.Conn)
-// 			if tlscon.ConnectionState().HandshakeComplete {
+			// check TLS handshake
+			tlscon := conn.(*tls.Conn)
+			if tlscon.ConnectionState().HandshakeComplete {
 
-// 				// check valid uuid was returned as ServerName (sni)
-// 				clientApiKey, err = uuid.Parse(tlscon.ConnectionState().ServerName)
-// 				if err != nil {
-// 					cLog.Warn().Msg("client sent invalid uuid")
-// 					break
-// 				}
+				// check valid uuid was returned as ServerName (sni)
+				clientApiKey, err = uuid.Parse(tlscon.ConnectionState().ServerName)
+				if err != nil {
+					cLog.Warn().Msg("client sent invalid uuid")
+					break
+				}
 
-// 				// check valid api key
-// 				if isValidApiKey(clientApiKey) {
-// 					// update log context with client uuid
-// 					cLog = cLog.With().Str("apikey", clientApiKey.String()).Logger()
-// 					cLog.Info().Msg("client connected")
-// 					// if API is valid, then set clientAuthenticated to TRUE
-// 					clientAuthenticated = true
-// 				} else {
-// 					// if API is not valid, then kill the connection
-// 					cLog.Warn().Msg("client sent invalid api key")
-// 					break
-// 				}
+				// check valid api key
+				if isValidApiKey(clientApiKey) {
+					// update log context with client uuid
+					cLog = cLog.With().Str("apikey", clientApiKey.String()).Logger()
+					cLog.Info().Msg("client connected")
+					// if API is valid, then set clientAuthenticated to TRUE
+					clientAuthenticated = true
+				} else {
+					// if API is not valid, then kill the connection
+					cLog.Warn().Msg("client sent invalid api key")
+					break
+				}
 
-// 			} else {
-// 				// if TLS handshake is not complete, then kill the connection
-// 				cLog.Warn().Msg("data received before tls handshake")
-// 				break
-// 			}
-// 		}
+			} else {
+				// if TLS handshake is not complete, then kill the connection
+				cLog.Warn().Msg("data received before tls handshake")
+				break
+			}
+		}
 
-// 		// If the client has been authenticated, then we can do stuff with the data
-// 		if clientAuthenticated {
-// 			// TODO: do stuff with the data - talk to boxie
-// 			// TODO: need a nice way to update atc that the feeder is online since the time it connected...
-// 			// TODO: maybe have a timer so that it only updates every 5 minutes + some random seconds (to prevent overload of ATC)
-// 			// TODO: do we also need to mark offline on disconnect?
-// 			// cLog.Debug().Msgf("data received: %s", fmt.Sprint(buf[:n]))
+		// If the client has been authenticated, then we can do stuff with the data
+		if clientAuthenticated {
+			// TODO: do stuff with the data - talk to boxie
+			// TODO: need a nice way to update atc that the feeder is online since the time it connected...
+			// TODO: maybe have a timer so that it only updates every 5 minutes + some random seconds (to prevent overload of ATC)
+			// TODO: do we also need to mark offline on disconnect?
+			// cLog.Debug().Msgf("data received: %s", fmt.Sprint(buf[:n]))
 
-// 			// get feeder lat/long
-// 			atcUrl, err := url.Parse(ctx.String("atcurl"))
-// 			if err != nil {
-// 				log.Error().Msg("--atcurl is invalid")
-// 				continue
-// 			}
-// 			s := atc.Server{
-// 				Url:      *atcUrl,
-// 				Username: ctx.String("atcuser"),
-// 				Password: ctx.String("atcpass"),
-// 			}
-// 			// refLat, refLon, err := atc.GetFeederLatLon(&s, clientApiKey)
-// 			_, _, err = atc.GetFeederLatLon(&s, clientApiKey)
-// 			if err != nil {
-// 				log.Err(err).Msg("atc.GetFeederLatLon")
-// 				continue
-// 			}
+			// get feeder lat/long
+			atcUrl, err := url.Parse(ctx.String("atcurl"))
+			if err != nil {
+				log.Error().Msg("--atcurl is invalid")
+				continue
+			}
+			s := atc.Server{
+				Url:      *atcUrl,
+				Username: ctx.String("atcuser"),
+				Password: ctx.String("atcpass"),
+			}
+			// refLat, refLon, err := atc.GetFeederLatLon(&s, clientApiKey)
+			_, _, err = atc.GetFeederLatLon(&s, clientApiKey)
+			if err != nil {
+				log.Err(err).Msg("atc.GetFeederLatLon")
+				continue
+			}
 
-// 			// at this point we should have everything we need to set up a producer for pw_ingest...
+			// at this point we should have everything we need to set up a producer for pw_ingest...
 
-// 			// set up producer
-// 			// producerOpts := make([]producer.Option, 3)
-// 			// producerOpts[0] = producer.WithSourceTag(clientApiKey.String())
-// 			// producerOpts[1] = producer.WithType(producer.Beast)
-// 			// producerOpts[2] = producer.WithPrometheusCounters(prometheusInputAvrFrames, prometheusInputBeastFrames, prometheusInputSbs1Frames)
-// 			// producerOpts = append(producerOpts, producer.WithReferenceLatLon(refLat, refLon))
+			// set up producer
+			// producerOpts := make([]producer.Option, 3)
+			// producerOpts[0] = producer.WithSourceTag(clientApiKey.String())
+			// producerOpts[1] = producer.WithType(producer.Beast)
+			// producerOpts[2] = producer.WithPrometheusCounters(prometheusInputAvrFrames, prometheusInputBeastFrames, prometheusInputSbs1Frames)
+			// producerOpts = append(producerOpts, producer.WithReferenceLatLon(refLat, refLon))
 
-// 		}
-// 	}
-// }
+		}
+	}
+}
 
 func main() {
 
@@ -299,7 +301,7 @@ func runServer(ctx *cli.Context) error {
 			continue
 		}
 		defer conn.Close()
-		// go clientConnection(ctx, conn, &tlsConfig)
+		go clientConnection(ctx, conn, &tlsConfig)
 	}
 	return nil
 }
