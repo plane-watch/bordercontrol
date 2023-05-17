fcb58dc016bf53a11cd01328f679e95358dc220d
Mike Nye <mike.nye@gmail.com>
2023-05-17 17:49:10 +0800

    .

--- pw_bordercontrol/cmd/bordercontrol/main.go
+++ pw_bordercontrol/cmd/bordercontrol/main.go
@@ -129,6 +129,7 @@ func startFeederContainers(ctx *cli.Context, containersToStart chan startContain
 		feederContainerName := fmt.Sprintf("feed-in-%s", containerToStart.uuid.String())
 		for _, container := range containers {
 			for _, cn := range container.Names {
+				log.Info().Str("looking", fmt.Sprintf("/%s", feederContainerName)).Str("cn", cn)
 				if cn == fmt.Sprintf("/%s", feederContainerName) {
 					foundContainer = true
 					break
