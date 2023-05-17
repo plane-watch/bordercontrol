9c50815f9c1855d4dc7d6e2972a5d3c08af7375b
Mike Nye <mike.nye@gmail.com>
2023-05-17 23:19:49 +0800

    .

--- pw_bordercontrol/cmd/bordercontrol/main.go
+++ pw_bordercontrol/cmd/bordercontrol/main.go
@@ -131,7 +131,7 @@ func checkFeederContainers(ctx *cli.Context) {
 
 		// check containers are running latest feed-in image
 		if container.Image != ctx.String("feedinimage") {
-			cfcLog.Info().Str("container", container.Names[0]).Msg("out of date container being killed for recreation")
+			cfcLog.Info().Str("container", container.Names[0][1:]).Msg("out of date container being killed for recreation")
 			err := cli.ContainerRemove(dockerCtx, container.ID, types.ContainerRemoveOptions{Force: true})
 			if err != nil {
 				cfcLog.Err(err).Str("container", container.Names[0]).Msg("could not kill out of date container")
