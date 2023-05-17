cbe792e7300744e43117b15601a1c30ff86ab663
Mike Nye <mike.nye@gmail.com>
2023-05-17 23:29:50 +0800

    .

--- pw_bordercontrol/cmd/bordercontrol/main.go
+++ pw_bordercontrol/cmd/bordercontrol/main.go
@@ -375,7 +375,7 @@ func clientConnection(ctx *cli.Context, connIn net.Conn, tlsConfig *tls.Config, 
 					cLog.Err(err).Msg("could not perform lookup")
 				}
 				for _, ip := range dstIPs {
-					cLog.Debug().IPAddr("ip", ip).Msg("could not perform lookup")
+					cLog.Debug().IPAddr("ip", ip).Msg("lookup results")
 				}
 
 				cLog.Debug().Str("dst", dialAddress).Msg("attempting to connect")
