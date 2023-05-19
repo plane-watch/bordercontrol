ee7e7c008a699ca7e7f186f05c2116d2e2e9bcfb
Mike Nye <mike.nye@gmail.com>
2023-05-19 09:35:12 +0800

    add healthcheck to feed-in containers

--- TODO.md
+++ TODO.md
@@ -1,8 +1,6 @@
 # todo
 
-- add mlat server to muxes
-- add mlat to bordercontrol
-- add dynamic reload of ssl certs
 - automatic feed-in container killing (healthcheck?)
 - add statistics
 - add web ui with stats & options (eg: kill feeder)
+- update online status of feeders
--- pw_feed-in/rootfs/scripts/healthcheck.sh
+++ pw_feed-in/rootfs/scripts/healthcheck.sh
@@ -1,3 +1,42 @@
+#!/usr/bin/with-contenv bash
+# shellcheck shell=bash
 
-# check beast connection inbound
-ss -ntH state established | tr -s " " | cut -d " " -f 3 | grep ":12345"
+EXITCODE=0
+
+# check beast connection inbound from bordercontrol
+CONNECTED_BEAST_IN=false
+if ss -ntH state established | tr -s " " | cut -d " " -f 3 | grep ":12345" > /dev/null 2>&1; then
+    CONNECTED_BEAST_IN=true
+    echo "CONNECTED_BEAST_IN=true"
+    EXITCODE=1
+fi
+
+# check beast connection outbound to mux
+CONNECTED_BEAST_OUT=false
+if ss -ntH state established | tr -s " " | cut -d " " -f 4 | grep ":12345" > /dev/null 2>&1; then
+    CONNECTED_BEAST_OUT=true
+    echo "CONNECTED_BEAST_OUT=true"
+    EXITCODE=1
+fi
+
+# check nats connection outbound
+CONNECTED_NATS_OUT=false
+if ss -ntH state established | tr -s " " | cut -d " " -f 4 | grep ":4222" > /dev/null 2>&1; then
+    CONNECTED_NATS_OUT=true
+    echo "CONNECTED_NATS_OUT=true"
+    EXITCODE=1
+fi
+
+# update /run/healthcheck
+if [ "$(cat /run/healthcheck)" != "$EXITCODE" ]; then
+    echo "$EXITCODE" > /run/healthcheck
+fi
+
+# if container has been unhealthy for 10mins+ then stop container
+if [ "$(cat /run/healthcheck)" != "0" ]; then
+    if test "$(find /blort -mmin +10)"; then
+        s6-svscanctl -t /var/run/s6/services
+    fi
+fi
+
+exit ${EXITCODE}
