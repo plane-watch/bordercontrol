1903bd6c9e45241f41d56c270cc22984f4fee277
Mike Nye <mike.nye@gmail.com>
2023-06-01 16:08:12 +0800

    simplify and split out large functions

--- pw-bordercontrol/cmd/bordercontrol/feeder_conn.go
+++ pw-bordercontrol/cmd/bordercontrol/feeder_conn.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/google/uuid"
@@ -14,7 +15,7 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
-func authenticateFeeder(ctx *cli.Context, connIn net.Conn, log zerolog.Logger, containersToStart chan startContainerRequest) (clientApiKey uuid.UUID, refLat, refLon float64, mux, label string, err error) {
+func authenticateFeeder(ctx *cli.Context, connIn net.Conn, log zerolog.Logger) (clientApiKey uuid.UUID, refLat, refLon float64, mux, label string, err error) {
 	// authenticates a feeder
 
 	// check TLS handshake
@@ -60,6 +61,190 @@ func authenticateFeeder(ctx *cli.Context, connIn net.Conn, log zerolog.Logger, c
 		return clientApiKey, refLat, refLon, mux, label, err
 	}
 	return clientApiKey, refLat, refLon, mux, label, err
+}
+
+func clientMLATConnection(ctx *cli.Context, clientConn net.Conn, tlsConfig *tls.Config) {
+	// handles incoming MLAT connections
+	// TODO: need a way to kill a client connection if the UUID is no longer valid (ie: feeder banned)
+
+	cLog := log.With().Str("listener", "MLAT").Logger()
+
+	var (
+		sendRecvBufferSize    = 256 * 1024 // 256kB
+		clientAuthenticated   = false
+		muxContainerConnected = false
+		muxConn               *net.TCPConn
+		muxConnErr            error
+		clientApiKey          uuid.UUID
+		mux                   string
+		bytesRead             int
+		err                   error
+	)
+
+	// update log context with client IP
+	remoteIP := net.ParseIP(strings.Split(clientConn.RemoteAddr().String(), ":")[0])
+	cLog = cLog.With().IPAddr("src", remoteIP).Logger()
+
+	// cLog.Debug().Msgf("connection established")
+	// defer cLog.Debug().Msgf("connection closed")
+
+	inBuf := make([]byte, sendRecvBufferSize)
+
+	for {
+
+		// read data from client
+		bytesRead, err = clientConn.Read(inBuf)
+		if err != nil {
+			if err.Error() == "tls: first record does not look like a TLS handshake" {
+				cLog.Warn().Msg(err.Error())
+				e := clientConn.Close()
+				if e != nil {
+					log.Err(e).Caller().Msg("could not close clientConn")
+				}
+				break
+			} else if err.Error() == "EOF" {
+				if clientAuthenticated {
+					cLog.Info().Msg("client disconnected")
+				}
+				e := clientConn.Close()
+				if e != nil {
+					log.Err(e).Caller().Msg("could not close clientConn")
+				}
+				break
+			} else {
+				cLog.Err(err).Msg("client read error")
+				e := clientConn.Close()
+				if e != nil {
+					log.Err(e).Caller().Msg("could not close clientConn")
+				}
+				break
+			}
+		}
+
+		// When the first data is sent, the TLS handshake should take place.
+		// Accordingly, we need to track the state...
+		if !clientAuthenticated {
+			clientApiKey, _, _, mux, _, err = authenticateFeeder(ctx, clientConn, cLog)
+			if err != nil {
+				cLog.Err(err)
+				break
+			}
+			clientAuthenticated = true
+		}
+
+		// If the client has been authenticated, then we can do stuff with the data
+		if clientAuthenticated {
+
+			// If we aren't yet connected to a mux
+			if !muxContainerConnected {
+
+				// attempt to connect to the mux container
+				dialAddress := fmt.Sprintf("%s:12346", mux)
+
+				var dstIP net.IP
+				dstIPs, err := net.LookupIP(mux)
+				if err != nil {
+					cLog.Err(err).Msg("could not perform lookup")
+				} else {
+					if len(dstIPs) > 0 {
+						dstIP = dstIPs[0]
+					} else {
+						continue
+					}
+				}
+
+				dstTCPAddr := net.TCPAddr{
+					IP:   dstIP,
+					Port: 12346,
+				}
+
+				// cLog.Debug().Str("dst", dialAddress).Msg("attempting to connect")
+				muxConn, muxConnErr = net.DialTCP("tcp", nil, &dstTCPAddr)
+
+				if muxConnErr != nil {
+
+					// handle connection errors to feed-in container
+
+					cLog.Warn().AnErr("error", muxConnErr).Str("dst", dialAddress).Msg("could not connect to mux container")
+					time.Sleep(1 * time.Second)
+
+					e := clientConn.Close()
+					if e != nil {
+						log.Err(e).Caller().Msg("could not close clientConn")
+					}
+					break
+
+				} else {
+
+					// connected OK...
+
+					err := muxConn.SetKeepAlive(true)
+					if err != nil {
+						cLog.Err(err).Msg("could not set keep alive")
+						e := clientConn.Close()
+						if e != nil {
+							log.Err(e).Caller().Msg("could not close clientConn")
+						}
+						break
+					}
+					err = muxConn.SetKeepAlivePeriod(1 * time.Second)
+					if err != nil {
+						cLog.Err(err).Msg("could not set keep alive period")
+						e := clientConn.Close()
+						if e != nil {
+							log.Err(e).Caller().Msg("could not close clientConn")
+						}
+						break
+					}
+
+					muxContainerConnected = true
+
+					// update stats
+					stats.setClientConnected(clientApiKey, clientConn.RemoteAddr(), "MLAT")
+					defer stats.setClientDisconnected(clientApiKey, "MLAT")
+
+					cLog = cLog.With().Str("dst", dialAddress).Logger()
+					cLog.Info().Msg("connected to mux")
+
+					// update stats
+					stats.setOutputConnected(clientApiKey, "MUX", muxConn.RemoteAddr())
+
+				}
+			}
+		}
+		// if we are ready to output data to the feed-in container...
+		if clientAuthenticated {
+			if muxContainerConnected {
+				break
+			}
+		}
+	}
+
+	// if we are ready to output data to the feed-in container...
+	if clientAuthenticated {
+		if muxContainerConnected {
+
+			wg := sync.WaitGroup{}
+
+			// write outstanding data
+			_, err := muxConn.Write(inBuf[:bytesRead])
+			if err != nil {
+				cLog.Err(err).Msg("error writing to client")
+			}
+
+			// start responder
+			wg.Add(1)
+			go mlatTcpForwarderM2C(clientApiKey, muxConn, clientConn, sendRecvBufferSize, cLog, &wg)
+			wg.Add(1)
+			go mlatTcpForwarderC2M(clientApiKey, clientConn, muxConn, sendRecvBufferSize, cLog, &wg)
+			wg.Wait()
+
+			defer muxConn.Close()
+			defer clientConn.Close()
+
+		}
+	}
+	// cLog.Debug().Msg("clientMLATConnection goroutine finishing")
 }
 
 func clientBEASTConnection(ctx *cli.Context, connIn net.Conn, containersToStart chan startContainerRequest) {
@@ -109,7 +294,7 @@ func clientBEASTConnection(ctx *cli.Context, connIn net.Conn, containersToStart 
 		// When the first data is sent, the TLS handshake should take place.
 		// Accordingly, we need to track the state...
 		if !clientAuthenticated {
-			clientApiKey, refLat, refLon, mux, label, err = authenticateFeeder(ctx, connIn, cLog, containersToStart)
+			clientApiKey, refLat, refLon, mux, label, err = authenticateFeeder(ctx, connIn, cLog)
 			if err != nil {
 				cLog.Err(err)
 				break
