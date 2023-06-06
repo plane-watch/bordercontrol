591e0c293d776bc4e689248917f06b99c351ef2a
Mike Nye <mike.nye@gmail.com>
2023-06-06 13:39:42 +0800

    update prom metrics

--- pw-bordercontrol/cmd/bordercontrol/prometheus.go
+++ pw-bordercontrol/cmd/bordercontrol/prometheus.go
@@ -9,41 +9,94 @@ import (
 
 // prometheus metrics
 var (
-	totalConnectionsBEAST = promauto.NewCounterFunc(prometheus.CounterOpts{
-		Name: "total_connections_beast",
+	promTotalConnectionsBEAST = promauto.NewGaugeFunc(prometheus.GaugeOpts{
+		Name: "bordercontrol_total_connections_beast",
 		Help: "The total number of active BEAST protocol connections being handled by this instance of bordercontrol.",
 	},
 		func() float64 {
 			stats.mu.RLock()
 			defer stats.mu.RUnlock()
-			numConns := float64(0)
+			n := float64(0)
 			for u, _ := range stats.Feeders {
-				numConns += float64(stats.Feeders[u].Connections["BEAST"].ConnectionCount)
+				n += float64(stats.Feeders[u].Connections["BEAST"].ConnectionCount)
 			}
-			return numConns
+			return n
 		})
 
-	totalConnectionsMLAT = promauto.NewCounterFunc(prometheus.CounterOpts{
-		Name: "total_connections_mlat",
+	promTotalConnectionsMLAT = promauto.NewGaugeFunc(prometheus.GaugeOpts{
+		Name: "bordercontrol_total_connections_mlat",
 		Help: "The total number of active MLAT protocol connections being handled by this instance of bordercontrol.",
 	},
 		func() float64 {
 			stats.mu.RLock()
 			defer stats.mu.RUnlock()
-			numConns := float64(0)
+			n := float64(0)
 			for u, _ := range stats.Feeders {
-				numConns += float64(stats.Feeders[u].Connections["MLAT"].ConnectionCount)
+				n += float64(stats.Feeders[u].Connections["MLAT"].ConnectionCount)
 			}
-			return numConns
+			return n
 		})
 
-	totalFeeders = promauto.NewCounterFunc(prometheus.CounterOpts{
-		Name: "total_feeders",
+	promTotalFeeders = promauto.NewGaugeFunc(prometheus.GaugeOpts{
+		Name: "bordercontrol_total_feeders",
 		Help: "The total number of feeders configured in ATC (active and inactive).",
 	},
 		func() float64 {
 			validFeeders.mu.RLock()
 			defer validFeeders.mu.RUnlock()
 			return float64(len(validFeeders.Feeders))
+		})
+
+	promActiveFeeders = promauto.NewGaugeFunc(prometheus.GaugeOpts{
+		Name: "bordercontrol_active_feeders",
+		Help: "The total number of feeders with an active connection to this instance of bordercontrol.",
+	},
+		func() float64 {
+			stats.mu.RLock()
+			defer stats.mu.RUnlock()
+			n := float64(0)
+			for u, _ := range stats.Feeders {
+				for p, _ := range stats.Feeders[u].Connections {
+					if stats.Feeders[u].Connections[p].Status == true {
+						n++
+						break
+					}
+				}
+			}
+			return n
+		})
+
+	promActiveFeedersBEAST = promauto.NewGaugeFunc(prometheus.GaugeOpts{
+		Name: "bordercontrol_active_feeders_beast",
+		Help: "The total number of feeders with an active BEAST protocol connection to this instance of bordercontrol.",
+	},
+		func() float64 {
+			stats.mu.RLock()
+			defer stats.mu.RUnlock()
+			n := float64(0)
+			for u, _ := range stats.Feeders {
+				if stats.Feeders[u].Connections["BEAST"].Status == true {
+					n++
+					break
+				}
+			}
+			return n
+		})
+
+	promActiveFeedersMLAT = promauto.NewGaugeFunc(prometheus.GaugeOpts{
+		Name: "bordercontrol_active_feeders_mlat",
+		Help: "The total number of feeders with an active MLAT protocol connection to this instance of bordercontrol.",
+	},
+		func() float64 {
+			stats.mu.RLock()
+			defer stats.mu.RUnlock()
+			n := float64(0)
+			for u, _ := range stats.Feeders {
+				if stats.Feeders[u].Connections["MLAT"].Status == true {
+					n++
+					break
+				}
+			}
+			return n
 		})
 )
