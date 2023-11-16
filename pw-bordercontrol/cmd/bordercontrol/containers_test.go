bb4808b7999e989a8d71a61f3755af02efe062ca
Mike Nye <mike.nye@gmail.com>
2023-11-16 20:45:51 +0800

    tests

--- pw-bordercontrol/cmd/bordercontrol/containers_test.go
+++ pw-bordercontrol/cmd/bordercontrol/containers_test.go
@@ -36,7 +36,7 @@ var (
 	TestDaemon *daemon.Daemon
 )
 
-func TestContainers(t *testing.T) {
+func TestContainersWithKill(t *testing.T) {
 
 	var (
 		ContainerEnvVarFeederLatOK                bool
@@ -174,6 +174,148 @@ func TestContainers(t *testing.T) {
 
 	// ensure container has been killed
 	t.Log("ensure container has been killed")
+	_, err = cli.ContainerInspect(*ctx, startedContainer.containerID)
+	assert.Error(t, err)
+
+}
+
+func TestContainersWithoutKill(t *testing.T) {
+
+	var (
+		ContainerEnvVarFeederLatOK                bool
+		ContainerEnvVarFeederLonOK                bool
+		ContainerEnvVarFeederUUIDOK               bool
+		ContainerEnvVarFeederReadsbNetConnectorOK bool
+		ContainerEnvVarFeederPWIngestSinkOK       bool
+
+		ContainerNetworkOK bool
+	)
+
+	// set logging to trace level
+	zerolog.SetGlobalLevel(zerolog.TraceLevel)
+
+	// starting test docker daemon
+	t.Log("starting test docker daemon")
+	TestDaemon = daemon.New(
+		t,
+		daemon.WithContainerdSocket(TestDaemonDockerSocket),
+	)
+	TestDaemon.Start(t)
+
+	// prep testing client
+	t.Log("prep testing client")
+	getDockerClient = func() (ctx *context.Context, cli *client.Client, err error) {
+		log.Debug().Msg("using test docker client")
+		cctx := context.Background()
+		cli = TestDaemon.NewClientT(t, client.WithAPIVersionNegotiation())
+		return &cctx, cli, nil
+	}
+
+	// clean up
+	defer func() {
+		t.Log("cleaning up")
+		TestDaemon.Stop(t)
+		TestDaemon.Cleanup(t)
+	}()
+
+	// continually send sighup1 to prevent checkFeederContainers from sleeping
+	testChan := make(chan os.Signal)
+	go func() {
+		for {
+			testChan <- syscall.SIGUSR1
+			time.Sleep(time.Second)
+		}
+	}()
+
+	// prepare channel for container start requests
+	containersToStartRequests := make(chan startContainerRequest)
+	defer close(containersToStartRequests)
+
+	// prepare channel for container start responses
+	containersToStartResponses := make(chan startContainerResponse)
+	defer close(containersToStartResponses)
+
+	// start process to test
+	t.Log("starting startFeederContainers")
+	go startFeederContainers(TestFeedInImageName, TestPWIngestSink, containersToStartRequests, containersToStartResponses)
+
+	// start container
+	t.Log("requesting container start")
+	containersToStartRequests <- startContainerRequest{
+		clientDetails: &feederClient{
+			clientApiKey: uuid.MustParse(TestFeederAPIKey),
+			refLat:       TestFeederLatitude,
+			refLon:       TestFeederLongitude,
+			mux:          TestFeederMux,
+			label:        TestFeederMux,
+		},
+		srcIP: net.IPv4(127, 0, 0, 1),
+	}
+
+	// wait for container to start
+	t.Log("waiting for container start")
+	startedContainer := <-containersToStartResponses
+
+	// ensure container started without error
+	t.Log("ensure container started without error")
+	assert.NoError(t, startedContainer.err)
+
+	// get docker client to inspect container
+	t.Log("get docker client to inspect container")
+	ctx, cli, err := getDockerClient()
+	assert.NoError(t, err)
+
+	// inspect container
+	t.Log("inspecting container")
+	ct, err := cli.ContainerInspect(*ctx, startedContainer.containerID)
+
+	// check environment variables
+	t.Log("checking container environment variables")
+	for _, e := range ct.Config.Env {
+		switch e {
+		case fmt.Sprintf("FEEDER_LAT=%f", TestFeederLatitude):
+			ContainerEnvVarFeederLatOK = true
+		case fmt.Sprintf("FEEDER_LON=%f", TestFeederLongitude):
+			ContainerEnvVarFeederLonOK = true
+		case fmt.Sprintf("FEEDER_UUID=%s", TestFeederAPIKey):
+			ContainerEnvVarFeederUUIDOK = true
+		case fmt.Sprintf("READSB_NET_CONNECTOR=%s,12345,beast_out", TestFeederMux):
+			ContainerEnvVarFeederReadsbNetConnectorOK = true
+		case fmt.Sprintf("PW_INGEST_SINK=%s", TestPWIngestSink):
+			ContainerEnvVarFeederPWIngestSinkOK = true
+		}
+	}
+	assert.True(t, ContainerEnvVarFeederLatOK)
+	assert.True(t, ContainerEnvVarFeederLonOK)
+	assert.True(t, ContainerEnvVarFeederUUIDOK)
+	assert.True(t, ContainerEnvVarFeederReadsbNetConnectorOK)
+	assert.True(t, ContainerEnvVarFeederPWIngestSinkOK)
+
+	// check container autoremove set to true
+	t.Log("check container autoremove set to true")
+	assert.True(t, ct.HostConfig.AutoRemove)
+
+	// check container network connection
+	t.Log("check container network connection")
+	for network, _ := range ct.NetworkSettings.Networks {
+		if network == feedInContainerNetwork {
+			ContainerNetworkOK = true
+		}
+	}
+	assert.Len(t, ct.NetworkSettings.Networks, 1)
+	assert.True(t, ContainerNetworkOK)
+
+	// test checkFeederContainers
+	t.Log("running checkFeederContainers")
+	err = checkFeederContainers("foo", testChan)
+	assert.NoError(t, err)
+
+	// wait for container to hopefully not be removed
+	t.Log("wait for container to hopefully not be removed")
+	time.Sleep(time.Second * 15)
+
+	// ensure container has been killed
+	t.Log("ensure container has not been killed")
 	_, err = cli.ContainerInspect(*ctx, startedContainer.containerID)
 	assert.NoError(t, err)
 
