9a624fda7eed7c3244f46dbc19180c0c9ee048cb
Mike Nye <mike.nye@gmail.com>
2023-11-17 11:15:34 +0800

    TestDFWTB

--- pw-bordercontrol/cmd/bordercontrol/main_test.go
+++ pw-bordercontrol/cmd/bordercontrol/main_test.go
@@ -8,8 +8,8 @@ import (
 )
 
 func TestDFWTB(t *testing.T) {
-	// poor attempt at humour
+	// don't mess with the banner!
 	bannerCheckSum := sha256.Sum256([]byte(banner))
 	expectedCheckSum := [32]uint8{28, 39, 253, 10, 28, 108, 170, 133, 71, 150, 147, 107, 235, 39, 187, 141, 112, 229, 54, 58, 2, 39, 205, 10, 136, 172, 42, 112, 13, 56, 182, 97}
-	assert.Equal(t, expectedCheckSum, bannerCheckSum)
+	assert.Equal(t, expectedCheckSum, bannerCheckSum, "don't mess with the banner! :-)")
 }
