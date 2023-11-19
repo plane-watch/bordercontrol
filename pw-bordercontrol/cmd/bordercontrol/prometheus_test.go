988f3c72af28461e5bf2fa7031149b927bb3cb78
Mike Nye <mike.nye@gmail.com>
2023-11-19 20:45:37 +0800

    test prom

--- pw-bordercontrol/cmd/bordercontrol/prometheus_test.go
+++ pw-bordercontrol/cmd/bordercontrol/prometheus_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 	"testing"
 	"time"
 
@@ -33,8 +34,30 @@ func TestProm(t *testing.T) {
 	assert.NoError(t, err)
 	fmt.Printf("client: got response!\n")
 	fmt.Printf("client: status code: %d\n", res.StatusCode)
-	body, err := io.ReadAll(res.Body)
+	bodyBytes, err := io.ReadAll(res.Body)
 	assert.NoError(t, err)
-	fmt.Println(string(body))
+	body := string(bodyBytes)
+
+	expectedMetrics := []string{
+		`pw_bordercontrol_connections{protocol="beast"} %d`,
+		`pw_bordercontrol_connections{protocol="mlat"} %d`,
+		`pw_bordercontrol_data_in_bytes_total{protocol="beast"} %d`,
+		`pw_bordercontrol_data_in_bytes_total{protocol="mlat"} %d`,
+		`pw_bordercontrol_data_out_bytes_total{protocol="beast"} %d`,
+		`pw_bordercontrol_data_out_bytes_total{protocol="mlat"} %d`,
+		`pw_bordercontrol_feedercontainers_image_current %d`,
+		`pw_bordercontrol_feedercontainers_image_not_current %d`,
+		`pw_bordercontrol_feeders %d`,
+		`pw_bordercontrol_feeders_active{protocol="beast"} %d`,
+		`pw_bordercontrol_feeders_active{protocol="mlat"} %d`,
+	}
+
+	// tests
+	for _, expectedMetric := range expectedMetrics {
+		assert.Equal(t,
+			strings.Count(body, fmt.Sprintf(expectedMetric, 0)),
+			1,
+		)
+	}
 
 }
