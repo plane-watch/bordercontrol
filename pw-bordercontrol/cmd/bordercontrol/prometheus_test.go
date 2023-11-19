18b921e47088cfadd41c193b649730bb36391c5b
Mike Nye <mike.nye@gmail.com>
2023-11-19 20:32:24 +0800

    work on TestProm

--- pw-bordercontrol/cmd/bordercontrol/prometheus_test.go
+++ pw-bordercontrol/cmd/bordercontrol/prometheus_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 	"testing"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -12,15 +13,22 @@ import (
 
 func TestProm(t *testing.T) {
 
-	// start server
+	// start metrics server
 	srv, err := nettest.NewLocalListener("tcp4")
 	assert.NoError(t, err)
 	err = srv.Close()
 	assert.NoError(t, err)
-
 	http.Handle("/metrics", promhttp.Handler())
-	http.ListenAndServe(srv.Addr().String(), nil)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		err = http.ListenAndServe(srv.Addr().String(), nil)
+		assert.NoError(t, err)
+		wg.Done()
+	}()
+	wg.Wait()
 
+	// request metrics
 	requestURL := fmt.Sprintf("http://%s", srv.Addr().String())
 	res, err := http.Get(requestURL)
 	assert.NoError(t, err)
