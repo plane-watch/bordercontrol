9e2456ee85b6e57483d7ea9e9bd24f03c5ef2e49
Mike Nye <mike.nye@gmail.com>
2023-11-19 20:25:31 +0800

    work on TestProm

---
+++ pw-bordercontrol/cmd/bordercontrol/prometheus_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/net/nettest"
+)
+
+func TestProm(t *testing.T) {
+
+	// start server
+	srv, err := nettest.NewLocalListener("tcp4")
+	assert.NoError(t, err)
+	err = srv.Close()
+	assert.NoError(t, err)
+
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(srv.Addr().String(), nil)
+
+	requestURL := fmt.Sprintf("http://%s", srv.Addr().String())
+	res, err := http.Get(requestURL)
+	assert.NoError(t, err)
+	fmt.Printf("client: got response!\n")
+	fmt.Printf("client: status code: %d\n", res.StatusCode)
+
+}
