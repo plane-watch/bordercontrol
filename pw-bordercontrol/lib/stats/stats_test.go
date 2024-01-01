ea1e1e1f672010dd6c08ae3744ad258ab1d56b9e
Mike Nye <mike.nye@gmail.com>
2024-01-01 22:41:53 +0800

    remove debugging

--- pw-bordercontrol/lib/stats/stats_test.go
+++ pw-bordercontrol/lib/stats/stats_test.go
@@ -293,10 +293,6 @@ func TestStats(t *testing.T) {
 		testURL := fmt.Sprintf("http://%s/metrics", testAddr)
 		body := getMetricsFromTestServer(t, testURL)
 
-		fmt.Println("---- BEGIN RESPONSE BODY ----")
-		fmt.Println(body)
-		fmt.Println("---- END RESPONSE BODY ----")
-
 		expectedMetrics := []string{
 			`pw_bordercontrol_connections{protocol="beast"} 0`,
 			`pw_bordercontrol_connections{protocol="mlat"} 0`,
