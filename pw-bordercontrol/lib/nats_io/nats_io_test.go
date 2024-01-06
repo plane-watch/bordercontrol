91a9843633eed4afb75290b95e9a056f64ba9d54
Mike Nye <mike.nye@gmail.com>
2024-01-06 21:23:47 +0800

    add coverage

--- pw-bordercontrol/lib/nats_io/nats_io_test.go
+++ pw-bordercontrol/lib/nats_io/nats_io_test.go
@@ -1,6 +1,7 @@
 package nats_io
 
 import (
+	"errors"
 	"strconv"
 	"strings"
 	"testing"
@@ -10,6 +11,7 @@ import (
 	"golang.org/x/net/nettest"
 
 	natsserver "github.com/nats-io/nats-server/v2/server"
+	"github.com/nats-io/nats.go"
 )
 
 func RunTestServer() (*natsserver.Server, error) {
@@ -39,16 +41,41 @@ func RunTestServer() (*natsserver.Server, error) {
 	// start nats server
 	server.Start()
 	if !server.ReadyForConnections(time.Second * 5) {
-		panic("NATS server didn't start")
+		return &natsserver.Server{}, errors.New("NATS server didn't start")
 	}
 	return server, nil
 }
 
 func TestNats(t *testing.T) {
 
-	// test isInitialised
-	t.Run("isInitialised", func(t *testing.T) {
-		require.False(t, initialised)
+	t.Run("test functions before initialise", func(t *testing.T) {
+
+		t.Run("isInitialised", func(t *testing.T) {
+			require.False(t, initialised)
+		})
+
+		t.Run("GetInstance", func(t *testing.T) {
+			_, err := GetInstance()
+			require.Error(t, err)
+			require.Equal(t, ErrNatsNotInitialised.Error(), err.Error())
+		})
+
+		t.Run("ThisInstance", func(t *testing.T) {
+			_, _, err := ThisInstance("*")
+			require.Error(t, err)
+			require.Equal(t, ErrNatsNotInitialised.Error(), err.Error())
+		})
+
+		t.Run("IsConnected", func(t *testing.T) {
+			require.False(t, IsConnected())
+		})
+
+		t.Run("Sub", func(t *testing.T) {
+			err := Sub("test", func(msg *nats.Msg) {})
+			require.Error(t, err)
+			require.Equal(t, ErrNatsNotInitialised.Error(), err.Error())
+		})
+
 	})
 
 	// start nats server
