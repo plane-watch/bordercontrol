3307d6b3f5b617b8f8a3728b24d56b305a3fb499
Mike Nye <mike.nye@gmail.com>
2024-01-09 20:50:24 +0800

    test Close

--- pw-bordercontrol/lib/stats/stats_test.go
+++ pw-bordercontrol/lib/stats/stats_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"golang.org/x/net/nettest"
 )
 
@@ -669,5 +670,10 @@ func TestStats(t *testing.T) {
 		_, ok = stats.Feeders[TestFeederAPIKey]
 		assert.False(t, ok)
 
+	})
+
+	t.Run("Close", func(t *testing.T) {
+		err := Close()
+		require.NoError(t, err)
 	})
 }
